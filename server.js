0eea48d6e559dacb028fadfe926a868b805230b0
AlCast <alvaro.castelan.21@gmail.com>
2018-06-28 10:11:23 -0500

    have server serving

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001; 
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+app.use(express.static("client/build"));
 
 app.get("/",(req,res)=>{
     res.send("hi");
@@ -15,8 +18,13 @@ app.get("/api/test",(req,res)=>{
     res.json(true); 
 });
 app.post("/api/test", (req,res)=>{
-console.log(req.body)
-res.json("true");
+console.log(req.body);
+req.body.received = true;
+res.json(req.body);
+})
+
+app.use(function(req,res){
+    res.sendFile(path.join(__dirname,"client/build/index.html"));
 })
 
 app.listen(PORT, function(){
